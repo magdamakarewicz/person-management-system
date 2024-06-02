79e519167f3646c073402fb53e517c29fa0cd9a9
magdalenamakarewicz <makarewicz.magdalena@outlook.com>
2024-06-02 13:09:18 +0200

    Add entity classes and configure relationships, update application.properties file

---
+++ dictionary-service/src/main/java/com/enjoythecode/dictionaryservice/model/Dictionary.java
@@ -0,0 +1,31 @@
+package com.enjoythecode.dictionaryservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Dictionary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_dictionary")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "dictionary")
+    private Set<DictionaryValue> dictionaryValues = new HashSet<>();
+
+    public Dictionary(String name) {
+        this.name = name;
+    }
+
+}
---
+++ dictionary-service/src/main/java/com/enjoythecode/dictionaryservice/model/DictionaryValue.java
@@ -0,0 +1,35 @@
+package com.enjoythecode.dictionaryservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class DictionaryValue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_dictionary_value")
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "dictionary_id")
+    private Dictionary dictionary;
+
+    public DictionaryValue(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
