fb18df67dae9fa89468ec1de73eff675b8ca210c
magdalenamakarewicz <makarewicz.magdalena@outlook.com>
2024-06-03 10:06:45 +0200

    Refactor SecurityFilterChain to replace deprecated methods with current API

--- user-service/src/main/java/com/enjoythecode/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/com/enjoythecode/userservice/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.enjoythecode.userservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,16 +21,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.POST, "/api/users").hasRole("ADMIN")
-                .anyRequest().permitAll()
-                .and()
-                .httpBasic();
-        return http.build();
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/users").hasRole("ADMIN")
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 
     @Bean
